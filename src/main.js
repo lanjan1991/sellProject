a8931df1847ea536dc5c19721533de4a2df502c9
allen <allen@orientalgame99.com>
2017-07-23 07:17:05 -0400

    5-4 vue-router (有bug)

--- src/main.js
+++ src/main.js
@@ -1,9 +1,18 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 import App from './App';
+import goods from 'components/goods/goods';
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  template: '<App/>',
-  components: { App }
+Vue.use(VueRouter);
+
+let app = Vue.extend(App);
+
+let router = new VueRouter();
+
+router.map({
+  '/goods': {
+    component: goods
+  }
 });
+
+router.start(app, '#app');
