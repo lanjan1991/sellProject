e8d00303982f734691952a843e61333f1e9dbedf
allen <allen@orientalgame99.com>
2017-07-23 04:47:04 -0400

    5-3 组件拆分

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,26 @@
 <template>
   <div id="app">
-    <!--<img src="./assets/logo.png">-->
-    <router-view></router-view>
+    <v-header></v-header>
+    <div class="tab">
+      I am tab
+    </div>
+    <div class="content">
+      I am content
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  import header from './components/header/header.vue';
+
+  export default{
+    components: {
+      'v-header': header
+    }
+  };
+
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
