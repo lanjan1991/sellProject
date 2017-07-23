b2764ac09c6df444d658447683868a4d44ba2ffd
allen <allen@orientalgame99.com>
2017-07-23 05:03:09 -0400

    stylus

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
   <div id="app">
     <v-header></v-header>
     <div class="tab">
-      I am tab
+      <div class="tab-item">商品</div>
+      <div class="tab-item">评论</div>
+      <div class="tab-item">商家</div>
     </div>
     <div class="content">
       I am content
@@ -11,16 +13,24 @@
 </template>
 
 <script>
-  import header from './components/header/header.vue';
+import header from './components/header/header.vue';
 
-  export default{
-    components: {
-      'v-header': header
-    }
-  };
+export default {
+  components: {
+    'v-header': header
+  }
+};
 
 </script>
 
-<style>
-
+<style lang="stylus" rel="stylesheet/stylus">
+  #app
+    .tab
+      display:flex
+      width: 100%
+      height: 40px
+      line-height: 40px
+      .tab-item
+        flex: 1
+        text-align: center
 </style>
