9cd7cebce1e1b2e8c250eb620cbc939d6b828822
allen <allen@orientalgame99.com>
2017-07-26 00:50:51 -0400

    6-8 header.vue 背景图 模糊效果

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -23,8 +23,12 @@
             </div>
         </div>
         <div class="bulletin-wrapper">
-            <span class="bulletin-tile"></span><span class="bulletin-text">{{seller.bulletin}}</span>
+            <span class="bulletin-tile"></span>
+            <span class="bulletin-text">{{seller.bulletin}}</span>
             <i class="icon-keyboard_arrow_right"></i>
+        </div>
+        <div class="background">
+            <img :src="seller.avatar" width="100%" height="100%">
         </div>
     </div>
 </template>
@@ -46,8 +50,9 @@ export default {
     @import "../../common/stylus/mixin.styl"
 
     .header
+        position: relative
         color: #fff
-        background: #999
+        background: rgba(7, 17, 27, 0.5)
         .content-wrapper
             position: relative
             padding:24px 12px 18px 24px
@@ -146,4 +151,13 @@ export default {
                 font-size: 10px
                 right: 12px
                 top: 8px
+
+        .background
+            position: absolute
+            top: 0
+            left: 0
+            width: 100%
+            height: 100%
+            z-index: -1
+            filter: blur(10px)
 </style>
