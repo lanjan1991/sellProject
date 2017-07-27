8bca8b93bc4f829ecf5d48952eff6ae73c7760f4
allen <allen@orientalgame99.com>
2017-07-26 22:31:19 -0400

    7-2 左侧menu布局

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,7 +1,13 @@
 <template>
     <div class="goods">
         <div class="menu-wrapper">
-    
+            <ul>
+                <li v-for="item in goods" class="menu-item">
+                    <span class="text border-1px">
+                        <span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>{{item.name}}
+                    </span>
+                </li>
+            </ul>
         </div>
         <div class="foods-wrapper"></div>
     </div>
@@ -22,11 +28,12 @@ export default {
         };
     },
     created() {
+        this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
+
         this.$http.get('/api/goods').then((response) => {
             response = response.body;
             if (response.errno === ERR_OK) {
                 this.goods = response.data;
-                console.log(this.goods);
             }
         });
     }
@@ -34,6 +41,8 @@ export default {
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+    @import "../../common/stylus/mixin.styl"
+
     .goods
         display: flex
         position: absolute
@@ -45,6 +54,36 @@ export default {
             flex: 0 0 80px
             width: 80px
             background: #f3f5f7
+            .menu-item
+                display: table
+                height: 54px
+                width: 56px
+                line-height: 14px
+                padding: 0 12px
+                .icon
+                    display: inline-block
+                    vertical-align: top
+                    width: 12px
+                    height: 12px
+                    margin-right: 2px
+                    background-size: 12px 12px
+                    background-repeat: no-repeat
+                    &.decrease
+                        bg-image('decrease_3')
+                    &.discount
+                        bg-image('discount_3')
+                    &.guarantee
+                        bg-image('guarantee_3')
+                    &.invoice
+                        bg-image('invoice_3')
+                    &.special
+                        bg-image('special_3')
+                .text
+                    display: table-cell
+                    width: 56px
+                    vertical-align: middle
+                    border-1px(rgba(7,17,27,0.1))
+                    font-size: 12px
         .foods-wrapper
-            background: blue
+            flex: 1
 </style>
